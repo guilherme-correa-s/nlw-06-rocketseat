6aad409bff8c41145c46806cf2acc13dd4764ed6
Guilherme Correa <guilhermescorrea@hotmail.com.br>
2021-08-22 21:00:29 -0300

    add sqlite

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,46 @@
-import Modal from './modal.js'
+import Modal from "./modal.js";
 
-const modal = Modal()
+const modal = Modal();
 
-const modalTitle = document.querySelector('.modal h2')
-const modalDescription = document.querySelector('.modal p')
-const modalButton = document.querySelector('.modal button')
+const modalTitle = document.querySelector(".modal h2");
+const modalDescription = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
 
-const checkButtons = document.querySelectorAll('.actions a.check')
-const deleteButton = document.querySelectorAll('.actions a.delete')
+const checkButtons = document.querySelectorAll(".actions a.check");
+const deleteButton = document.querySelectorAll(".actions a.delete");
 
-checkButtons.forEach(button => {
-    button.addEventListener('click', (event) => {handleClick(event)})
-})
+checkButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    handleClick(event);
+  });
+});
 
-deleteButton.forEach(button => {
-    button.addEventListener('click', (event) => {handleClick(event, false)})
-})
+deleteButton.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    handleClick(event, false);
+  });
+});
 
 function handleClick(event, check = true) {
-    event.preventDefault()
+  event.preventDefault();
 
-    const slug = check ? "check" : "delete"
-    
-    const roomId = document.querySelector('#room-id').dataset.id
-    const form = document.querySelector('.modal form')
-    const questionId = event.target.dataset.id
+  const slug = check ? "check" : "delete";
 
-    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+  const roomId = document.querySelector("#room-id").dataset.id;
+  const form = document.querySelector(".modal form");
+  const questionId = event.target.dataset.id;
 
-    modalTitle.innerHTML = check ? "Marcar como lida essa pergunta" : "Excluir essa perguta"
-    modalDescription.innerHTML = check ? "Deseja marcar essa pergunta como lida?" : "Tem certeza que deseja excluir essa pergunta?"
-    modalButton.innerHTML = check ? "Sim, marcar como lida" : "Sim, excluir"
-    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
-    modal.open()
+  form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`);
+
+  modalTitle.innerHTML = check
+    ? "Marcar como lida essa pergunta"
+    : "Excluir essa perguta";
+  modalDescription.innerHTML = check
+    ? "Deseja marcar essa pergunta como lida?"
+    : "Tem certeza que deseja excluir essa pergunta?";
+  modalButton.innerHTML = check ? "Sim, marcar como lida" : "Sim, excluir";
+  check
+    ? modalButton.classList.remove("red")
+    : modalButton.classList.add("red");
+  modal.open();
 }
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,20 +1,19 @@
 export default function Modal() {
+  const modalWrapper = document.querySelector(".modal-wrapper");
+  const cancelButton = document.querySelector(".button.cancel");
 
-    const modalWrapper = document.querySelector('.modal-wrapper')
-    const cancelButton = document.querySelector('.button.cancel')
-    
-    cancelButton.addEventListener('click', close)
+  cancelButton.addEventListener("click", close);
 
-    function open() {
-        modalWrapper.classList.add('active')
-    }
+  function open() {
+    modalWrapper.classList.add("active");
+  }
 
-    function close() {
-        modalWrapper.classList.remove('active')
-    }
+  function close() {
+    modalWrapper.classList.remove("active");
+  }
 
-    return {
-        open,
-        close
-    }
+  return {
+    open,
+    close,
+  };
 }
